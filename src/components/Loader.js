a7d480929dfd84fdc64e9fd8381de9bd3ada93a7
smirnovsky <happy280190@gmail.com>
2023-05-15 19:01:06 +0300

    delete loading spinner. bugfix

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Stack } from '@mui/material'
-import { InfinitySpin } from 'react-loader-spinner'
 
 const Loader = () => {
   return (
     <Stack direction='row' justifyContent='center' alignItems='center' width='100%'>
-        <InfinitySpin color='gray' />
+        Loading...
     </Stack>
   )
 }
